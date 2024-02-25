8c161e7b8c324bdab7e3d2473960f2aa0ae21e18
APATARA1112 <alejandropatonaraujo2@gmail.com>
2024-02-25 19:24:59 +0100

    Creating EditorialFactory

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Factory\AutorFactory;
+use App\Factory\EditorialFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,6 +12,7 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         AutorFactory::createMany(200);
+        EditorialFactory::createMany(100);
 
         $manager->flush();
     }
