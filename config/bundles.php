0959454014609644a36232f12cc4178cbe022cdb
APATARA1112 <alejandropatonaraujo2@gmail.com>
2024-02-25 19:21:17 +0100

    Creating AutorFactory

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
