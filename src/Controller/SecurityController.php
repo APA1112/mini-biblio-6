95b0244b1c45b1dce13219b94528bce4127cac8a
Alejandro <apatara1112@g.educaand.es>
2024-03-07 09:58:24 +0100

    Implemented text with user email and buttons to login and logout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,12 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/entrar', name: 'app_login')]
-    public function login(): Response{
-        return $this->render('security/login.html.twig');
+    public function login(AuthenticationUtils $authenticationUtils): Response{
+        //Obtener el ultimo mensaje de error, si existe
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        //Obtenemos el ultimo nombre de usuario introducido
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username'=>$lastUsername, 'error'=>$error]);
     }
 
     #[Route(path: '/salir', name: 'app_logout')]
