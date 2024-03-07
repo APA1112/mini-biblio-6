b9d9a93a58361a5057d8c8518bee15de8c342498
Alejandro <apatara1112@g.educaand.es>
2024-03-07 08:44:19 +0100

    Modifing entity Libro to provide users

---
+++ migrations/Version20240307074131.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240307074131 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE libro CHANGE isbn isbn VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE socio ADD email VARCHAR(255) NOT NULL, ADD password VARCHAR(255) NOT NULL, ADD is_admin TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE libro CHANGE isbn isbn VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE socio DROP email, DROP password, DROP is_admin');
+    }
+}
--- src/Entity/Socio.php
+++ src/Entity/Socio.php
@@ -7,6 +7,8 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Context\ExecutionContext;
@@ -14,7 +16,7 @@ use Symfony\Component\Validator\Context\ExecutionContext;
 #[ORM\Entity(repositoryClass: SocioRepository::class)]
 #[ORM\Table(name: 'socio')]
 #[UniqueEntity(fields: 'dni', message: 'Ya existe un socio con ese DNI')]
-class Socio
+class Socio implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -37,7 +39,14 @@ class Socio
     #[ORM\Column(type: 'string', nullable: true)]
     private ?string $telefono;
 
+    #[ORM\Column(type: 'string')]
     private ?string $email;
+
+    #[ORM\Column(type: 'string')]
+    private ?string $password;
+
+    #[ORM\Column(type: 'boolean')]
+    private ?bool $isAdmin;
 
     public function __construct()
     {
@@ -114,13 +123,14 @@ class Socio
 
     public function addLibro(Libro $libro)
     {
-        if (!$this->libros->contains($libro)){
+        if (!$this->libros->contains($libro)) {
             $this->libros->add($libro);
             $libro->setSocio($this);
         }
     }
 
-    public function removeLibro(Libro $libro) {
+    public function removeLibro(Libro $libro)
+    {
         $this->libros->removeElement($libro);
         $libro->setSocio(null);
     }
@@ -136,17 +146,81 @@ class Socio
         return $this;
     }
 
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): Socio
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(?string $password): Socio
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    public function getIsAdmin(): ?bool
+    {
+        return $this->isAdmin;
+    }
+
+    public function setIsAdmin(?bool $isAdmin): Socio
+    {
+        $this->isAdmin = $isAdmin;
+        return $this;
+    }
+
+
     public function __toString(): string
     {
         return $this->getNombre() . ' ' . $this->getApellidos();
     }
 
-    public function validate(ExecutionContext $context, $payload) : void {
-        if(!$this->esDocente || !$this->esEstudiante){
+    public function validate(ExecutionContext $context, $payload): void
+    {
+        if (!$this->esDocente || !$this->esEstudiante) {
             $context->buildViolation('Un socio debe de ser estudiante, docente o ambos')
                 ->atPath('esEstudiante')
                 ->atPath('esDocente')
                 ->addViolation();
         }
+    }
+
+    public function getRoles(): array
+    {
+        $roles = [];
+        //Guarantee at least every user has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    public function eraseCredentials()
+    {
+        // In case you store any temporary, sensitive information
+    }
+
+    public function getUsername(): string
+    {
+        return (string)$this->email;
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return (string)$this->email;
     }
 }
